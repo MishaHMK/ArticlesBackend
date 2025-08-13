06a92f5646e1d0a6d333b6204a63cd688613e1ba
MykhailoHumeniuk <gumeniukmich@gmail.com>
2025-08-13 18:42:24 +0300

    data-connection-and-seed
    Created models, DB connection and Data Seeding

---
+++ src/main/java/com/microapp/articlesapp/model/Article.java
@@ -0,0 +1,51 @@
+package com.microapp.articlesapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@SQLDelete(sql = "UPDATE articles SET is_deleted = true,"
+        + "WHERE id = ?")
+@SQLRestriction(value = "is_deleted = false")
+@Table(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private OffsetDateTime publicationDate;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
