a20f71b4c6c26991e926f48842ec79d4b3a42c15
MykhailoHumeniuk <gumeniukmich@gmail.com>
2025-08-13 18:50:58 +0300

    unit-test-h2-fix

--- src/test/java/com/microapp/articlesapp/ArticlesAppApplicationTests.java
+++ src/test/java/com/microapp/articlesapp/ArticlesAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.microapp.articlesapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ArticlesAppApplicationTests {
 
     @Test
